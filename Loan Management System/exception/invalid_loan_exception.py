6f03c50d7f5e3e1ef3503a1425b9e34774135930
Chirag Bhatia <94846496+ChiragBhatia24@users.noreply.github.com>
2024-10-17 15:07:59 +0530

    Delete Loan Management System directory

--- Loan Management System/exception/invalid_loan_exception.py
+++
@@ -1,9 +0,0 @@
-class CustomerNotFoundException(Exception):
-    def __init__(self, message="CustomerNotFoundException"):
-        self.message = message
-        super().__init__(self.message)
-
-class LoanNotFoundException(Exception):
-    def __init__(self, message="LoanNotFoundException"):
-        self.message = message
-        super().__init__(self.message)
--- Loan Management System/util/db_property_util.py
+++
@@ -1,8 +0,0 @@
-class DBPropertyUtil:
-    @staticmethod
-    def get_db_conn_str():
-        return ('Driver={ODBC Driver 17 for SQL Server};'
-                'Server=DESKTOP-RJ2AKHB\\SQLEXPRESS;'
-                'Database=;'
-                'Trusted_Connection=yes;')
-            
